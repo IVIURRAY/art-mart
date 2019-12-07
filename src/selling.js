f7bc83d18fd50874a1fc700810de83acc29e28fd
IVIURRAY <haydn.murray1@yahoo.co.uk>
2019-12-07 21:39:14 +0000

    flesh out selling page

--- src/selling.js
+++ src/selling.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useReducer } from 'react'
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
 import { Text, Button } from 'react-native-elements';
 import { withNavigation } from 'react-navigation';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
-import API, { graphqlOperation } from '@aws-amplify/api'
+import AppHeader from './appHeader';
 
 class Selling extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: '',
+            description: '',
+            price: 0
+        }
+    }
+
     componentDidMount() {
         this.getPermissionAsync();
-        console.log('hi');
     }
 
     getPermissionAsync = async () => {
@@ -33,12 +41,30 @@ class Selling extends React.Component {
     }
 
     render() {
+        console.log(this.state);
         return (
             <View style={styles.container}>
-                <Button
-                    title="Pick an image from camera roll"
-                    onPress={this._pickImage}
-                />
+                <View style={styles.imageContainer}>
+                    <Button
+                        title="Chose image"
+                        onPress={this._pickImage}
+                    />
+                </View>
+                <View style={styles.titleContainer}>
+                    <TextInput defaultValue="Title" onChangeText={val => this.setState({ title: val })} />
+                </View>
+                <View style={styles.descriptionContainer}>
+                    <TextInput defaultValue="Description" onChangeText={val => this.setState({ description: val })} />
+                </View>
+                <View style={styles.priceContainer}>
+                    <TextInput defaultValue="10.00" keyboardType="numeric" onChangeText={val => this.setState({ price: val })} />
+                </View>
+                <View style={styles.uploadContainer}>
+                    <Button
+                        title="List product"
+                        onPress={this._uploadProduct}
+                    />
+                </View>
             </View>
         )
     }
@@ -47,9 +73,26 @@ class Selling extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#ddeeff',
+        backgroundColor: '#ffffff',
         alignItems: 'center',
         justifyContent: 'center',
+        flex: 1
+    },
+    imageContainer: {
+        flex: 6,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    titleContainer: {
+        flex: 1
+    },
+    descriptionContainer: {
+        flex: 1
+    },
+    priceContainer: {
+        flex: 1
+    },
+    uploadContainer: {
         flex: 1
     }
 });
